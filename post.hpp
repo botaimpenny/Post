53fd2b179a08363cb56af798c3f3b36b378226bd
milanacharonova <popelnitckiy2@gmail.com>
2025-05-15 22:04:52 +0300

    Update post.hpp

--- post.hpp
+++ post.hpp
@@ -6,20 +6,39 @@
 namespace PACK {
 class post {
     std::string _name;
-    int _index//Индекс почтового отделения
+    int _index;//Индекс почтового отделения
     int _x;//Координата отделения по х
     int _y;//Координата отделения по у
     std::vector <int> _packs;//Список посылок внутри отделения
 public:
     post() = default;
     post(int x, int y, std::vector <int> packs);
-    friend std::ostream& operator << (std::ostream & out, post& p);
-    friend std::istream& operator >> (std::istream & in, post& p);
+    friend std::ostream& operator<<(std::ostream &out, const post &p);
+    friend std::istream& operator>>(std::istream &in, post &p);
     void display();
     void delpost();
     void addpost();
     double distance(pack* arr, int count);  // теперь принимает id
     double calculateRemainingTime(pack* arr, int count, int id); // Новая функция для расчета времени
+};
+std::ostream& operator<<(std::ostream &out, const post &p) {
+    out << p._name << " " << p._index << " " << p._x << " " << p._y << " ";
+    out << p._packs.size() << " ";
+    for (int val : p._packs) {
+        out << val << " ";
+    }
+    return out;
 }
-std::ostream& operator << (std::ostream & out, const pack& p);
-std::istream& operator >> (std::istream & in, pack& p);
+
+std::istream& operator>>(std::istream &in, post &p) {
+    p._packs.clear();
+    int packs_count;
+
+    in >> p._name >> p._index >> p._x >> p._y >> packs_count;
+    for (int i = 0; i < packs_count; ++i) {
+        int val;
+        in >> val;
+        p._packs.push_back(val);
+    }
+    return in;
+}
